ef9db8aa208a77d3b9479aa709338326a0f797e9
ValeriaCaptain7 <pantera3072@gmail.com>
2022-12-11 23:29:12 +0100

    Update apiUrl

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ function displayTemperature(response) {
 
 function search(city) {
     let apiKey = "49ecec9d222e3fb8o4502323fetf4ac1";
-    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Rome&key=49ecec9d222e3fb8o4502323fetf4ac1&units=metric";
+    let apiUrl =
+      `https://api.shecodes.io/weather/v1/current?query={Rome}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 }
 
