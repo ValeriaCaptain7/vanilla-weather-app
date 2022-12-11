877275155e1b7cb226857f827cc63cc9296428d7
ValeriaCaptain7 <pantera3072@gmail.com>
2022-12-11 19:17:50 +0100

    Javascript city and temperature.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("Hello");
+function displayTemperature(response) {
+
+    console.log(response.data.city);
+    console.log(response.data.temperature.current);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    temperatureElement.innerHTML = Math.round (response.data.temperature.current);
+    cityElement.innerHTML = (response.data.city);
+}
+
+
+let apiKey = "49ecec9d222e3fb8o4502323fetf4ac1";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Rome&key=49ecec9d222e3fb8o4502323fetf4ac1&units=metric";
+
+  
+  axios.get(apiUrl).then(displayTemperature);
+
