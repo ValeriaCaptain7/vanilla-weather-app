b273af1e6dec7f93f54d46c68fc7ca0cdba3fc58
ValeriaCaptain7 <pantera3072@gmail.com>
2022-12-12 20:05:42 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ function displayTemperature(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
-    iconElement.setAttribute("alt", response.data.condition.description);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
